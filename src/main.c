7c6e422e83cfc7210f0ae5e621a7522f5abd660e
Mystia <zhang1997yang@outlook.com>
2020-04-17 16:40:29 +0800

    0417 add usart

--- src/main.c
+++ src/main.c
@@ -1,10 +1,11 @@
-#include "STC15Fxxxx.H"
-#include "soft_uart.h"
+#include "C:\\Users\\Administrator\\Documents\\Source\\MCU\\Sdcc-51\\src\\STC15Fxxxx.H"
+#include "C:\\Users\\Administrator\\Documents\\Source\\MCU\\Sdcc-51\\src\\USART.h"
 #include <intrins.h>
-#include "EEPROM.h"
-#include "D:\\SDK\\msc51\\adc.h"
+#include "C:\\Users\\Administrator\\Documents\\Source\\MCU\\Sdcc-51\\src\\EEPROM.h"
+#include "C:\\Users\\Administrator\\Documents\\Source\\MCU\\Sdcc-51\\src\\adc.h"
 
 static int Maxact=3;
+static u8 Makeup1,Makeup2;
 
 sbit X0 = P1^2;
 sbit X1 = P1^3;
@@ -21,17 +22,19 @@ int Act4();	//小大大小
 
 void AdcInit();
 int AdcExcute();
-
+int UARTInit();
+int UARTRead();
+int EEPROMend();
 
 
 
 void  delay_ms(unsigned int ms)
 {
-     unsigned int i;
-	 do{
-	      i = 12000000L / 13000;
-		  while(--i)	;   //14T per loop
-     }while(--ms);
+    unsigned int i;
+	do{
+	    i = 12000000L / 13000;
+		while(--i)	;   //14T per loop
+    }while(--ms);
 }
 
 u16 result1=0;
@@ -40,27 +43,23 @@ u8 act;
 u8 n=0;
 int i=0;
 u8 X3X=1;
+u16 addr;
+
 int main()
 {
 	AdcInit();
 	delay_ms(100);
-	Get_ADC10bitResult(0);
-	delay_ms(100);
-	result1=Get_ADC10bitResult(0);
-	delay_ms(100);
-	Get_ADC10bitResult(1);
-	delay_ms(100);
-	result2=Get_ADC10bitResult(1);
+	
 	if(result1==1024)
 	{
 		while(1)
 		{
 			delay_ms(200);
-			TxSend(0x99);
+			//TxSend(0x99);
 			delay_ms(50);
-			TxSend((u8)result1);
+			//TxSend((u8)result1);
 			delay_ms(50);
-			TxSend((u8)result2);
+			//TxSend((u8)result2);
 		}
 	}
 	if(result2==1024)
@@ -68,15 +67,15 @@ int main()
 		while(1)
 		{
 			delay_ms(200);
-			TxSend(0x99);
+			//TxSend(0x99);
 			delay_ms(50);
-			TxSend((u8)result1);
+			//TxSend((u8)result1);
 			delay_ms(50);
-			TxSend((u8)result2);
+			//TxSend((u8)result2);
 		}
 	}
-	result1=result1-500;
-	result2=result2-400;
+	EEPROM_read_n(0x0010,&Makeup1,1);
+	EEPROM_read_n(0x11,&Makeup2,1);
 	EEPROM_read_n(0x0000,&act,1);
 	if(act>Maxact)act=0;
 	P1=0XFF;
@@ -106,6 +105,7 @@ int main()
 				default:
 					break;
 				}
+				AdcExcute();
 			}
 			if(X3==0)
 			{
@@ -121,6 +121,8 @@ int main()
 			}
 		}
 		X3X=X3;
+		EEPROMend();
+		UARTRead();
 	}
 }
 		
@@ -247,5 +249,73 @@ void AdcInit()
 
 int AdcExcute()
 {
+	Get_ADC10bitResult(0);
+	delay_ms(50);
+	result1=Get_ADC10bitResult(0);
+	delay_ms(50);
+	Get_ADC10bitResult(1);
+	delay_ms(50);
+	result2=Get_ADC10bitResult(1);
+	result1=result1-Makeup1;
+	result2=result2-Makeup2;
+}
 
+
+int UARTInit()
+{
+	COMx_InitDefine		COMx_InitStructure;					
+	COMx_InitStructure.UART_Mode      = UART_8bit_BRTx;		
+	COMx_InitStructure.UART_BRT_Use   = BRT_Timer1;			
+	COMx_InitStructure.UART_BaudRate  = 115200ul;			
+	COMx_InitStructure.UART_RxEnable  = ENABLE;				
+	COMx_InitStructure.BaudRateDouble = DISABLE;			
+	COMx_InitStructure.UART_Interrupt = ENABLE;				
+	COMx_InitStructure.UART_Polity    = PolityLow;			
+	COMx_InitStructure.UART_P_SW      = UART1_SW_P30_P31;	
+	COMx_InitStructure.UART_RXD_TXD_Short = DISABLE;		
+	USART_Configuration(USART1, &COMx_InitStructure);		
+}
+
+
+int UARTRead()
+{
+	if(COM1.RX_TimeOut > 0)	
+	{
+		if(--COM1.RX_TimeOut == 0)
+		{
+			switch (RX1_Buffer[0])
+			{
+			case 'D':
+				IAP_CONTR=0x60;
+				break;
+			case 'u':
+				Makeup1+=10;
+				break;
+			case 'i':
+				Makeup1-+10;
+				break;
+			case 'j':
+				Makeup2+=10;
+				break;
+			case 'k':
+				Makeup2-+10;
+				break;
+			default:
+				break;
+			}
+
+			COM1.RX_Cnt = 0;
+		}
+	}
+}
+
+int EEPROMend()
+{
+	EEPROM_SectorErase(0x00);
+	delay_ms(5);
+	EEPROM_write_n(0x10,&Makeup1,1);
+	delay_ms(5);
+	EEPROM_write_n(0x11,&Makeup2,1);
+	delay_ms(5);
+	EEPROM_write_n(0x00,&act,1);
 }
